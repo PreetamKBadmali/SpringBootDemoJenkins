2aabef8cfb487432f6d402d4f72195ca40b0a401
MSI <MSI@192.168.1.35>
2024-12-25 21:40:58 +0530

    SourceCodeCommit

---
+++ src/main/java/com/example/demo/StudentController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//http://localhost:8080/student/hello
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello, World!";
+	}
+}
