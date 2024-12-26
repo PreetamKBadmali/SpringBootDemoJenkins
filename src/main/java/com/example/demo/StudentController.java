c75d10d2c0b4cfc4d6a9af96d7b44bec1b3b2d74
MSI <MSI@192.168.1.35>
2024-12-26 11:56:26 +0530

    commit fro springboot jenkin depooyment

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 	@GetMapping("/hello")
 	public String sayHello() {
-		return "Hello, World!";
+		return "Hello, World!!";
 	}
 }
